adb2f3a664399efdfa9b1947ad7b2786d98df929
Sl0wYx <lol-waring14@mail.ru>
2026-04-01 01:16:35 +0100

    Add terraform

---
+++ main.tf
@@ -0,0 +1,188 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+
+  # Uncomment this if you are using student account
+  # skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "minecraft_student"
+  location = "francecentral"
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = "minecraft-tunnel-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet1" {
+    name                  = "subnet1"
+    resource_group_name   = azurerm_resource_group.rg.name
+    virtual_network_name  = azurerm_virtual_network.vnet.name
+    address_prefixes      = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "minecraft-tunnel-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.my_ip
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "minecraft-bedrock"
+    priority                   = 320
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = "1423"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "map"
+    priority                   = 330
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "cloud"
+    priority                   = 340
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "7580"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "voice"
+    priority                   = 350
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = "25508"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "nginx"
+    priority                   = 370
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "test"
+    priority                   = 380
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "25570"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_managed_disk" "disk" {
+  name                 = "minecraft-tunnel_disk1"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Premium_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "30"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "minecraft-tunnel-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "minecraft-tunnel"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_B2ats_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "debian"
+    offer     = "debian-11"
+    sku       = "11-gen2"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "minecraft-tunnel_disk1_cdd39a53273149819ead25b5ceef9a19"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+    disk_size_gb    = 30
+  }
+  os_profile {
+    computer_name  = "minecraft-tunnel"
+    admin_username = "noboobs"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys{
+      key_data = var.ssh_key
+        path     = "/home/noboobs/.ssh/authorized_keys"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "my_ip" {
+  type        = string
+}
+
+variable "ssh_key" {
+    type        = string
+}
